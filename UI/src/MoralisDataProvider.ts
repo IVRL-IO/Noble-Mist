0312ca3ed7f053f5e973645d390f32bb8f0e8e06
Robert M. Meffe <865593+RobertMeffe@users.noreply.github.com>
2022-03-24 17:11:25 -0400

    Rewrote all the code now website and electron both build....
    I should have had a V8 this morning.
    http://whatthecommit.com/9682b9b84270d2f9525760fbc2d7e165

---
+++ UI/src/MoralisDataProvider.ts
@@ -0,0 +1,8 @@
+/* Moralis init code */
+import Moralis from "moralis";
+
+const serverUrl = "https://imefijc31bnp.usemoralis.com:2053/server";
+const appId = "M62YTVf3pvP2HZvErtUHnYoc8OGwMjNQKeG03Nm4";
+await Moralis.start({ serverUrl, appId });
+
+/* TODO: Add Moralis Authentication code */
---
+++ UI/src/components/Index.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="align-self-stretch bg-secondary">
+  Please log into access features
+</div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+export default defineComponent({
+  name: 'HelloWorld'
+})
+</script>
+
+<style scoped>
+
+</style>
